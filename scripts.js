157b82c36d55e5af0651cb049e5abf0073450593
defowler2005 <defowler2005@gmail.com>
2024-10-27 12:31:17 +1100

    D

--- scripts.js
+++ scripts.js
@@ -10,8 +10,6 @@ const defowler = {
      * @param {String} message - The text to be printet intto the console.
      */
     writeLog: (type, message) => {
-        let color;
-        let b_color;
         const validTypes = ['INFO', 'WARNING', 'ERROR'];
 
         if (!type || validTypes.includes(type) === false) return console.log(`%c[WARNING] - You must include a valid message type for the usage of writeLog(). Valid types include ${validTypes.join(', ')}.`, `color: #ffa365; background-color: #654b39;`);
@@ -20,42 +18,48 @@ const defowler = {
 
         switch (formattedType) {
             case 'INFO':
-                b_color = '#485b34';
-                color = '#96d652';
+                console.log(`%c[${formattedType}] - ${message}`, `color: #96d652; background-color: #485b34`);
                 break;
             case 'WARNING':
-                b_color = '#654b39';
-                color = '#ffa365';
+                console.warn(`%c[${formattedType}] - ${message}`, `color: #ffa365; background-color: #654b39`);
                 break;
             case 'ERROR':
-                b_color = '#694143';
-                color = '#fe7b7f';
+                console.error(`%c[${formattedType}] - ${message}`, `color: #fe7b7f; background-color: #694143`);
                 break;
             default:
-                b_color = '#f8d7da';
-                color = '#96d652';
-                message = `Type ${type} is an invalid usage for the writeLog() function, types include: INFO, WARNING, ERROR`;
+                console.warn(`%c[${formattedType}] - Type ${type} is an invalid usage for the writeLog() function, types include: INFO, WARNING, ERROR`, `color: #ffa365; background-color: #654b39`);
                 break;
-        }; console.log(`%c[${formattedType}] - ${message}`, `color: ${color}; background-color: ${b_color}`);
+        };
     },
     /**
      * Dynamically redirect the user upon a nav button click.
      * @param {String} pageName - The page button the user clicked.
      */
-    handleNavClick: (pageName) => {
-        const allpageNames = ['homePage'];
-        
-        if (allpageNames.includes(pageName) === false) return defowler.writeLog('WARNING', 'An invalid ');
+    handleNavButton: (pageName) => {
+        const allpageNames = ['home', 'projects'];
+
+        if (allpageNames.includes(pageName) === false) return defowler.writeLog('WARNING', 'An invalid pageName was supplied at the handleNavButton() function.');
+
+        switch (pageName) {
+            case allpageNames[0]:
+                window.location.href = '/'; // Back to the root (index.html).
+                break;
+                case allpageNames[1]:
+                window.location.href = '/projects.html'; // Back to the root (index.html).
+                break;
+            default:
+                break;
+        }
     }
 };
 
-window.onload = () => {
-    const isNonComputer = /Mobile|Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
-    
-    if (isNonComputer) {
-        if (typeof eruda !== 'undefined') eruda.init();
-        else defowler.writeLog('WARN', 'Eruda could not be loaded, maybe because eruda was not able to be fetched with httpGet. Sorry phone users!');
-    } else {
-        defowler.writeLog('INFO', 'Eruda was not loaded as the user already has a console.');
-    }
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const hasConsole = /Mobile|Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
+
+    if (hasConsole) { // Does the client have a console?
+        if (typeof eruda !== 'undefined') {
+            eruda.init();
+            defowler.writeLog('INFO', 'Eruda successfully loaded you now have a console on your phone!');
+        } else defowler.writeLog('WARN', 'Eruda could not be loaded, eruda looks to be undefined. Sorry, phone users!');
+    } else defowler.writeLog('INFO', 'Eruda was not loaded as the user already has a console.');
+});
